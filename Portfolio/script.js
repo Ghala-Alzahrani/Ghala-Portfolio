0b6e8ff68d852ba0302fab65f51204d814e6f78c
galaa20300-code <galaa20300@gmail.com>
2026-06-20 02:50:23 +0300

    Delete Portfolio directory

--- Portfolio/script.js
+++
@@ -1,5 +0,0 @@
-const observer = new IntersectionObserver((entries) => {
-      entries.forEach(el => { if (el.isIntersecting) el.target.classList.add('visible'); });
-    }, { threshold: 0.1 });
-
-    document.querySelectorAll('.fade-in').forEach(el => observer.observe(el));
