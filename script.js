913b1322dafb322736e89f77c6858d977ae007c7
Ghala <galaa20300@gmail.com>
2026-06-20 05:15:08 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const observer = new IntersectionObserver((entries) => {
     }, { threshold: 0.1 });
 
     document.querySelectorAll('.fade-in').forEach(el => observer.observe(el));
+    
+
+
+    const toggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.nav-links');
+
+toggle.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
